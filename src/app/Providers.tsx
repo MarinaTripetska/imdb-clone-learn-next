177e7217b1a262dc0f82ab6e63dffdf71c864e90
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-08-01 13:29:38 +0200

    feat: add About page

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
 import React from "react";
+import { ThemeProvider } from "next-themes";
 
-const Providers = ({ children }) => {
+const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <div className="select-none dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen">
